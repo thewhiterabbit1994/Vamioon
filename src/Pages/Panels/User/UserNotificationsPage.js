ea3cd525cea35f45377fa2c38ffca476192697fd
Ho3ein Zarrabi <ho3einzarrabi@gmail.com>
2022-03-02 14:29:03 +0330

    User Notif Style Compeleted

--- src/Pages/Panels/User/UserNotificationsPage.js
+++ src/Pages/Panels/User/UserNotificationsPage.js
@@ -1,7 +1,11 @@
+import UserNotifHeader from "../../../Components/Panel/User/UserNotifications/UserNotifHeader";
+import UserNotifMain from "../../../Components/Panel/User/UserNotifications/UserNotifMain";
+
 export default () => {
   return (
-    <div>
-      <div>im user notifications</div>
+    <div className="h-[59vw]">
+      <UserNotifHeader />
+      <UserNotifMain />
     </div>
   );
 };
